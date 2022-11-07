de658af1fc6c8623c47506b7ba1c8f76500bc346
HlebSayanov <say4havi@gmail.com>
2022-11-07 14:34:59 +0300

    edit project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <div>
+            <a href="src/components/Header/Header#">Home</a>-
+            <a href="src/components/Header/Header#">News Feed</a>-
+            <a href="src/components/Header/Header#">Messages</a>-
+
+        </div>
+    )
+}
+
