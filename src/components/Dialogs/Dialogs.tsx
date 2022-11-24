4bb241cdd424efec81767dc9c268c6c29140a55d
HlebSayanov <say4havi@gmail.com>
2022-11-25 01:39:15 +0300

    add_editDialogs_21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,21 @@ import classes from "./Dialogs.module.css";
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.activeDialog}>Hleb</div>
+                <div className={classes.dialog}>Andrey</div>
+                <div className={classes.dialog}>Viktoryia</div>
+                <div className={classes.dialog}>Iryna</div>
+                <div className={classes.dialog}>Yuri</div>
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.messageUser}>Hello</div>
+                <div className={classes.messageUser}>Welcom to social network</div>
+                <div className={classes.messageUser}>How to go Cyprus</div>
+
+            </div>
         </div>
     );
 };
