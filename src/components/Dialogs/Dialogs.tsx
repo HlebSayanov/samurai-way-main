f7d5c682205eb663825a8d4859c6ffa561994880
HlebSayanov <say4havi@gmail.com>
2022-11-26 14:42:05 +0300

    add_excatRoute_22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 
 export const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog + ' ' + classes.activeDialog}>Hleb</div>
-                <div className={classes.dialog}>Andrey</div>
-                <div className={classes.dialog}>Viktoryia</div>
-                <div className={classes.dialog}>Iryna</div>
-                <div className={classes.dialog}>Yuri</div>
+                <div className={classes.dialog + ' ' + classes.activeDialog}>
+                  <NavLink to={'/dialogs/1'}>Hleb</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/2'}> Andrey</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/3'}> Viktoryia</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/4'}> Iryna</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/5'}> Yuri</NavLink>
+                </div>
             </div>
 
             <div className={classes.messages}>
